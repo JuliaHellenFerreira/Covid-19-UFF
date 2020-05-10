c7b8b71373021d86f00d9d57317e73dffb3334b3
Julia Hellen Ferreira <38631847+JuliaHellenFerreira@users.noreply.github.com>
2020-05-10 13:17:58 -0300

    Add files via upload

--- PoliticasRiodeJaneiro.R
+++ PoliticasRiodeJaneiro.R
@@ -25,23 +25,22 @@ download.file(url = paste0(Link,"/",File), destfile = File)
 load(File) 
 GET.UFF.READ(c("CASOS.CONFIRMADOS.RJ.R","OBITOS.RJ.R"))
 
-## Data da primeira medida de cada municipio - Tem erro!
+## Data da primeira medida de cada municipio
 
 Medidas <- Politicas_RiodeJaneiro_1_ %>%
   select(Municipio, Inicio, Classificao) %>%
   filter(Classificao != "Outros")
-View(Medidas)
 
-PrimeiraMedida <- function(x){
+PrimeiraMedida <- function(x){ #Não funciona
   linhas <- nrow(x)
   colunas <- ncol(x)
   d_medida <- data.frame(Municipio = c(NULL), Dia = c(NULL))
   t = 0
   for (i in 1:linhas){
     for (j in 1:colunas){
-      if (x[i,j] == "Medidas de Prevenção"){
+      if (x[i,j] == "Calamidade Pública"){
         t = t + 1
-        d_medida[t,1] = rownames(x)[i]
+        d_medida[t,1] = x[i,1]
         d_medida[t,2] = x[i,2]
         break
       }
@@ -61,9 +60,9 @@ PrimeiroCaso <- function(x){
   for (i in 1:linhas){
     for (j in  2:colunas){
       if (x[i,j] != 0){
-        t = t+  1
-        ocor[t,1] <- rownames(x)[i]
-        ocor [t,2] <- colnames(x)[j]
+        t = t +  1
+        ocor[t,1] = rownames(x)[i]
+        ocor [t,2] = colnames(x)[j]
         break
       }
     }
